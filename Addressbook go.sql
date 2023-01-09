5916c91582499559ee0e9422b724432f1d9c0607
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2023-01-09 18:49:59 +0530

    Add files via upload

---
+++ Addressbook go.sql
@@ -0,0 +1,40 @@
+show databases;
+create database AddressBook;
+use AddressBook;
+create table contact(
+address_id int not null ,
+firstname varchar(250) not null,
+lastname varchar(250) not null,
+address varchar(250) not null,
+city varchar(50) not null,
+state varchar(50) not null,
+phoneno int not null,
+email varchar(100)not null,
+primary key(address_id)
+);
+desc contact;
+
+insert into contact
+(address_id,firstname,lastname,address,city,state,phoneno,email)
+values
+(1,"Rose","Carter","2nd North Street","Pune","MAHA",987586241,"rose@mail.co"),
+(2,"Amy","Owen","Northhill","Nagpur","MAHA",988636541,"oamy@gmail.co"),
+(3,"Lucian","Stokes","F City","Jaipur","Rajasthan",85436541,"lstokes@gmail.com"),
+(4,"Arjun","Reddy","Moon City","Belgaon","Karnataka",8532331,"reddys@gmail.com") ;
+select * from contact;
+show tables;
+
+select * from contact
+where city="Pune";
+
+update contact
+set phoneno=775432188
+where state="MAHA";
+select * from contact;
+
+SET SQL_SAFE_UPDATES=0;
+
+update contact
+set address="Gorebandar"
+where city="Pune";
+select * from contact;
